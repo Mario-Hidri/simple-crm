abc7cdeebdc365580b9a16fe76cf0c38b1b342f3
Mario Hidri <mariohidri2014@yahoo.com>
2025-11-19 11:06:25 +0100

    added input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLine } from '@angular/material/core';
+import { MatLine, MatNativeDateModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +27,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTooltipModule,
     MatDialogModule,
     MatLine,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,   
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { MatDialogActions, MatDialogContent } from "@angular/material/dialog";
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogActions, MatDialogContent],
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+  ],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
-
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const day = cellDate.getDay();
+    return day === 0 ? 'sunday-date' : '';
+  };
 }
